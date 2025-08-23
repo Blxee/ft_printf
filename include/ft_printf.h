af5df1bb0f37dd0a213542dadbeb49e4cb3b3820
Blxee <aminedev57@gmail.com>
2025-08-23 23:39:04 +0100

    Added: progress of tokenizer

--- include/ft_printf.h
+++ include/ft_printf.h
@@ -7,6 +7,46 @@
 
 #include "str_utils.h"
 
+typedef enum e_specifier {
+	STRING,
+	CHAR,
+	DECIMAL = 10,
+	HEXADECIMAL = 16,
+	OCTAL = 8,
+	BINARY = 2,
+	FLOAT,
+}	t_specifier;
+
+typedef enum e_length {
+	BYTE,
+	UNSIGNED_BYTE,
+	SHORT,
+	UNSIGNED_SHORT,
+	INT,
+	UNSIGNED_INT,
+	LONG,
+	UNSIGNED_LONG,
+	LONG_LONG,
+	UNSIGNED_LONG_LONG,
+}	t_length;
+
+typedef enum e_align {
+	RIGHT,
+	LEFT,
+	CENTER,
+}	t_align;
+
+typedef struct s_token {
+	char		*str;
+	t_specifier	specifier;
+	t_length	length;
+	int			pad_len;
+	char		pad_char;
+	t_align		align;
+}	t_token;
+
+int ft_count_tokens(char *str);
+t_token *ft_parse_tokens(char *str);
 int ft_printf(char *format, ...);
 
 #endif
--- src/ft_printf.c
+++ src/ft_printf.c
@@ -1,5 +1,71 @@
 #include "ft_printf.h"
 
+int ft_count_tokens(char *str)
+{
+	int count;
+	int i;
+
+	count = 0;
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '%')
+			count++;
+		if (str[i + 1] == '%')
+			i++;
+		i++;
+	}
+	return (count);
+}
+
+t_token *ft_parse_conversion(char *str)
+{
+	t_token *result;
+	static char *specifier_keys = "sSpdDioOuUxXcC";
+	static t_specifier specifier_values[] = {STRING, STRING, CHAR, CHAR, DECIMAL, DECIMAL};
+	int i;
+	
+	result = malloc(sizeof(*result));
+	i = 0;
+	while (specifier_keys[i])
+	{
+		if (str[1] == specifier_keys[i])
+		{
+			result->specifier = specifier_values[i];
+			break ;
+		}
+		i++;
+	}
+	return (result);
+}
+
+t_token **ft_parse_tokens(char *str)
+{
+	t_token **result;
+	int count;
+	int i;
+	int j;
+
+	count = count_tokens(str);
+	result = malloc(sizeof(*result) * (count + 1));
+	i = 0;
+	j = 0;
+	while (i < count)
+	{
+		while (str[j] && str[j] != '%')
+		{
+			j++;
+		}
+		if (j > 0)
+		{
+			result[i] = ft_parse_conversion(str + i);
+			str[i + j] = '\0';
+		}
+		i++;
+	}
+	result
+}
+
 int ft_printf(char *format, ...)
 {
 	va_list args;
