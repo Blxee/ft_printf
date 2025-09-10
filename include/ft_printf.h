03bfe40e098f194b5566657b728ffb0f73958a2d
Blxee <aminedev57@gmail.com>
2025-09-10 16:14:53 +0100

    Changed: most types in ft_printf.h

--- include/ft_printf.h
+++ include/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: blxee <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/06 16:12:18 by blxee             #+#    #+#             */
-/*   Updated: 2025/09/06 16:22:34 by blxee            ###   ########.fr       */
+/*   Updated: 2025/09/10 15:59:12 by blxee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,23 +43,24 @@ typedef enum e_length {
 	UNSIGNED_LONG_LONG,
 }	t_length;
 
-typedef enum e_align {
-	RIGHT,
-	LEFT,
-	CENTER,
-}	t_align;
+typedef enum e_falg {
+	MINUS = '-',
+	PLUS = '+',
+	ZERO = '0',
+	SPACE = ' ',
+	HASH = '#',
+}	t_flag;
 
-typedef struct s_token {
-	char		*str;
-	t_specifier	specifier;
-	t_length	length;
-	int			pad_len;
-	char		pad_char;
-	t_align		align;
-}	t_token;
+typedef struct s_format {
+	t_flag				flags[32];
+	unsigned int	width;
+	unsigned int	precision;
+	t_length			length;
+	t_specifier		specifier;
+}	t_format;
 
 int ft_count_tokens(char *str);
-t_token *ft_parse_tokens(char *str);
+t_format *ft_parse_tokens(char *str);
 int ft_printf(char *format, ...);
 
 #endif
