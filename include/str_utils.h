694ddab94b6e4769c218b063d8d861e0ca61d625
Blxee <aminedev57@gmail.com>
2025-09-10 16:15:54 +0100

    Changed: all functions and added a new string type to str_utils.h

--- include/str_utils.h
+++ include/str_utils.h
@@ -1,10 +1,24 @@
 #ifndef STR_UTILS_H
 #define STR_UTILS_H
 
-void ft_putchar(char c);
-int ft_strlen(char *str);
-void ft_putstr(char *str);
-void ft_putnstr(char *str, int n);
-void ft_putnbr(long long nbr);
+#include <stdarg.h>
+
+#define STRING_INIT_CAPACITY (128)
+
+typedef struct s_string {
+  char  *str;
+  int   capacity;
+  int   length;
+} t_string;
+
+typedef struct s_format t_format;
+
+t_string *ft_str_create(void);
+void ft_str_add_char(t_string *const buf, t_format *format, va_list *list);
+void ft_str_add_str(t_string *const buf, t_format *format, va_list *list);
+void ft_str_add_nbr_base(t_string *const buf, t_format *format, va_list *list);
+void ft_str_add_float(t_string *const buf, t_format *format, va_list *list);
+void ft_str_add_scientific_notation(t_string *const buf, t_format *format, va_list *list);
+void ft_str_free(t_string **buf);
 
 #endif
