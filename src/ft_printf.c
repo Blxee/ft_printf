9168acb2760e7d107a665e74f7e33e1d4a368501
Blxee <aminedev57@gmail.com>
2025-09-17 14:33:33 +0100

    Changed: format parsing and printing is not in separate function

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,11 +6,22 @@
 /*   By: atahiri- <atahiri-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/24 23:23:27 by atahiri-          #+#    #+#             */
-/*   Updated: 2025/09/16 20:55:19 by atahiri-         ###   ########.fr       */
+/*   Updated: 2025/09/17 14:32:08 by atahiri-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+int ft_print_format(t_string *const buffer, const char *const format, va_list *const args)
+{
+	// mem_alloc(sizeof(t_format));
+	if (format[1] == 's')
+		ft_str_add_str(buffer, NULL, args);
+	else if (format[1] == 'c')
+		ft_str_add_char(buffer, NULL, args);
+
+	return (2);
+}
 
 int ft_printf(char *format, ...)
 {
@@ -32,14 +43,7 @@ int ft_printf(char *format, ...)
 			ft_str_extend(buffer, format + i, j);
 		}
 		if (format[i + j])
-		{
-
-			if (format[i + j + 1] == 's')
-				ft_str_add_str(buffer, NULL, &args);
-			else if (format[i + j + 1] == 'c')
-				ft_str_add_char(buffer, NULL, &args);
-			j += 2;
-		}
+			j += ft_print_format(buffer, format + i + j, &args);
 		i += j;
 	}
 	ft_putstr(buffer->str);
