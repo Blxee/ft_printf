fd7fc2b831267713086e028e6a208f0a25e54961
Amine Tahiri Alaoui <atahiri-@e2r2p13.1337.ma>
2025-10-21 09:21:02 +0100

    Added: basic working version of ft_sprintf

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,19 @@
 /*   By: atahiri- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 08:20:41 by atahiri-          #+#    #+#             */
-/*   Updated: 2025/10/21 08:25:12 by atahiri-         ###   ########.fr       */
+/*   Updated: 2025/10/21 09:18:46 by atahiri-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_printf(void)
+#include "ft_printf.h"
+
+int	ft_sprintf(char *str, const char *format, ...)
 {
-	return ;
+	va_list	ap;
+
+	va_start(ap, format);
+	ft_strlcpy(str, format, 12);
+	va_arg(ap, int);
+	va_end(ap);
+	return (0);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -6,11 +6,18 @@
 /*   By: atahiri- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 08:21:17 by atahiri-          #+#    #+#             */
-/*   Updated: 2025/10/21 08:21:20 by atahiri-         ###   ########.fr       */
+/*   Updated: 2025/10/21 08:46:18 by atahiri-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
+
+# include <stdarg.h>
+# include "libft/libft.h"
+
+int	ft_printf(const char *format, ...);
+int	ft_dprintf(int fd, const char *format, ...);
+int	ft_sprintf(char *str, const char *format, ...);
 
 #endif
