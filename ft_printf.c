05647e29946194f9566388a7d9c20777ce83b2fd
Amine Tahiri Alaoui <atahiri-@e2r3p13.1337.ma>
2025-10-25 08:40:14 +0100

    Added: basic char formatting

--- ft_printf.c
+++ ft_printf.c
@@ -6,28 +6,21 @@
 /*   By: atahiri- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 08:20:41 by atahiri-          #+#    #+#             */
-/*   Updated: 2025/10/23 09:24:34 by atahiri-         ###   ########.fr       */
+/*   Updated: 2025/10/25 08:39:39 by atahiri-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static const t_handler	g_handlers[] = {
-	&ft_str_extend_char,
-	&ft_str_extend_str,
-	&ft_str_extend_pointer,
-	&ft_str_extend_decimal,
-	&ft_str_extend_decimal,
-	&ft_str_extend_unsigned,
-};
-
-int	ft_sprintf(char *str, const char *format, ...)
+int		ft_dprintf(int fd, const char *format, ...)
 {
-	va_list					ap;
-	size_t					start;
-	size_t					len;
-	t_fmt					fmt;
+	va_list	ap;
+	t_fmt	fmt;
+	int start;
+	int len;
 
+	if (format == NULL)
+		return (-1);
 	va_start(ap, format);
 	start = 0;
 	len = 0;
@@ -35,121 +28,100 @@ int	ft_sprintf(char *str, const char *format, ...)
 	{
 		if (format[start + len] == '%')
 		{
-			ft_strlcat(str, format + start, len + 1);
-			start = len;
-			fmt = ft_parse_fmt(((char *)format) + start, &start);
-			g_handlers[fmt.type](str, fmt, &ap);
-			len = 0;
+			ft_putnstr_fd((char *)format + start, fd, len);
+			fmt = ft_parse_fmt((char *)format + start + len, &start);
+			fmt.handler(fd, fmt, &ap);
+			start += len;
 		}
 		else
 			len++;
 	}
-	ft_strlcat(str, format + start, len + 1);
+	ft_putstr_fd((char *)format + start, fd);
 	va_end(ap);
 	return (0);
 }
 
-t_fmt	ft_parse_fmt(char *str, size_t *offset)
+void	ft_putnstr_fd(char *s, int fd, int n)
 {
-	static const char		*s_types = "cspdiuxX%";
-	static const char		*s_lengths[] = {"hh", "h", "", "l", "ll", NULL};
-	t_fmt					fmt;
-	size_t					i;
-	size_t					len;
+	write(fd, s, n);
+}
 
-	str++;
+t_fmt	ft_parse_fmt(char *str, int *offset)
+{
+	static const char		*ident_keys = "cspdiuxX%";
+	static const t_handler	ident_vals[] = {
+		&ft_print_char,
+		&ft_print_str,
+		&ft_print_pointer,
+		&ft_print_decimal,
+		&ft_print_decimal,
+		&ft_print_unsigned,
+	};
+	t_fmt					fmt;
+	int						i;
+	int						off;
+
+	off = 1;
 	i = 0;
-	while (s_lengths[i] != NULL)
+	ft_bzero(&fmt, sizeof(fmt));
+	while (ident_keys[i] != '\0')
 	{
-		len = ft_strlen(s_lengths[i]);
-		if (ft_strncmp(s_lengths[i], str, len) == 0)
+		if (ident_keys[i] == str[off])
 		{
-			fmt.length = i;
+			fmt.handler = ident_vals[i];
 			break ;
 		}
 		i++;
 	}
-	str += len;
-	i = 0;
-	while (s_types[i] != '\0')
-	{
-		if (s_types[i] == *str)
-		{
-			fmt.type = i;
-			break ;
-		}
-		i++;
-	}
-	*offset += 2;
+	off++;
+	*offset += off;
 	return (fmt);
 }
 
-void	ft_str_extend_char(char *buf, t_fmt fmt, va_list *ap)
+int		ft_print_char(int fd, t_fmt fmt, va_list *ap)
 {
-	char	chr[2];
+	char	chr;
 
 	(void)fmt;
-	chr[0] = (char)va_arg(*ap, int);
-	chr[1] = '\0';
-	ft_strlcat(buf, chr, 1024);
+	chr = (char)va_arg(*ap, int);
+	ft_putchar_fd(chr, fd);
+	return (1);
 }
 
-void	ft_str_extend_str(char *buf, t_fmt fmt, va_list *ap)
+int		ft_print_str(int fd, t_fmt fmt, va_list *ap)
 {
 	char	*str;
 
 	(void)fmt;
 	str = va_arg(*ap, char *);
-	ft_strlcat(buf, str, 1024);
+	ft_putstr_fd(str, fd);
+	return (ft_strlen(str));
 }
 
-void	ft_str_extend_pointer(char *buf, t_fmt fmt, va_list *ap)
+int		ft_print_pointer(int fd, t_fmt fmt, va_list *ap)
 {
-	(void)buf;
+	(void)fd;
 	(void)fmt;
 	(void)ap;
+	return (-1);
 }
 
-void	ft_str_extend_decimal(char *buf, t_fmt fmt, va_list *ap)
+int		ft_print_decimal(int fd, t_fmt fmt, va_list *ap)
 {
-	long long	nbr;
-	char		*str;
+	int		nbr;
 
-	nbr = 0;
-	if (BYTE == fmt.length)
-		nbr = va_arg(*ap, int);
-	if (SHORT == fmt.length)
-		nbr = va_arg(*ap, int);
-	if (INT == fmt.length)
-		nbr = va_arg(*ap, int);
-	// if (LONG == fmt.length)
-	// 	nbr = va_arg(*ap, long);
-	// if (LONG_LONG == fmt.length)
-	// 	nbr = va_arg(*ap, long long);
-
-	str = ft_itoa(nbr);
-	ft_strlcat(buf, str, 1024);
-	free(str);
+	(void)fmt;
+	nbr = va_arg(*ap, int);
+	ft_putnbr_fd(nbr, fd);
+	return (-1);
 }
 
-void	ft_str_extend_unsigned(char *buf, t_fmt fmt, va_list *ap)
+int		ft_print_unsigned(int fd, t_fmt fmt, va_list *ap)
 {
 	unsigned long long	nbr;
-	char				*str;
 
-	nbr = 0;
-	if (BYTE == fmt.length)
-		nbr = va_arg(*ap, unsigned int);
-	if (SHORT == fmt.length)
-		nbr = va_arg(*ap, unsigned int);
-	// if (INT == fmt.length)
-	// 	nbr = va_arg(*ap, unsigned int);
-	// if (LONG == fmt.length)
-	// 	nbr = va_arg(*ap, long);
-	// if (LONG_LONG == fmt.length)
-	// 	nbr = va_arg(*ap, long long);
-
-	str = ft_itoa(nbr);
-	ft_strlcat(buf, str, 1024);
-	free(str);
+	(void)fmt;
+	nbr = va_arg(*ap, unsigned int);
+	ft_putnbr_fd(nbr, fd);
+	return (-1);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: atahiri- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 08:21:17 by atahiri-          #+#    #+#             */
-/*   Updated: 2025/10/23 09:23:15 by atahiri-         ###   ########.fr       */
+/*   Updated: 2025/10/24 18:10:46 by atahiri-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,45 +16,26 @@
 # include <stdarg.h>
 # include "libft/libft.h"
 
-typedef enum s_type {
-	CHAR,
-	STRING,
-	POINTER,
-	DECIMAL,
-	UNSIGNED,
-	HEX_LOWER,
-	HEX_UPPER,
-	PRECENT,
-}	t_type;
+typedef struct s_fmt t_fmt;
 
-typedef enum t_length {
-	BYTE,
-	SHORT,
-	INT,
-	LONG,
-	LONG_LONG,
-}	t_length;
+typedef int	(*t_handler)(int fd, t_fmt, va_list *);
 
 typedef struct s_fmt {
-	// falgs
+	// flags
 	// width
-	// precision
-	t_length length;
-	t_type	type;
+	t_handler handler;
 }	t_fmt;
 
-typedef void	(*t_handler)(char *, t_fmt, va_list *);
-
-int		ft_printf(const char *format, ...);
 int		ft_dprintf(int fd, const char *format, ...);
-int		ft_sprintf(char *str, const char *format, ...);
+int		ft_printf(const char *format, ...);
 
-t_fmt	ft_parse_fmt(char *str, size_t *offset);
+void	ft_putnstr_fd(char *s, int fd, int n);
+t_fmt	ft_parse_fmt(char *str, int *offset);
 
-void	ft_str_extend_char(char *buf, t_fmt fmt, va_list *ap);
-void	ft_str_extend_str(char *buf, t_fmt fmt, va_list *ap);
-void	ft_str_extend_pointer(char *buf, t_fmt fmt, va_list *ap);
-void	ft_str_extend_decimal(char *buf, t_fmt fmt, va_list *ap);
-void	ft_str_extend_unsigned(char *buf, t_fmt fmt, va_list *ap);
+int		ft_print_char(int fd, t_fmt fmt, va_list *ap);
+int		ft_print_str(int fd, t_fmt fmt, va_list *ap);
+int		ft_print_pointer(int fd, t_fmt fmt, va_list *ap);
+int		ft_print_decimal(int fd, t_fmt fmt, va_list *ap);
+int		ft_print_unsigned(int fd, t_fmt fmt, va_list *ap);
 
 #endif
